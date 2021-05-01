dc692556ac4c87a253b2eaff05ac8df7966a4278
Art S <art.worknsk@gmail.com>
2021-05-01 12:02:28 +0700

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { FeedPageComponent } from './feed-page/feed-page.component';
+import { JobsPageComponent } from './jobs-page/jobs-page.component';
+import { NetworkPageComponent } from './network-page/network-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProfilePageComponent},
+  {path: 'feed', component: FeedPageComponent},
+  {path: 'network', component: NetworkPageComponent},
+  {path: 'jobs', component: JobsPageComponent},
+  {path: 'chat', component: ChatPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { FeedPageComponent } from './feed-page/feed-page.component';
+import { NetworkPageComponent } from './network-page/network-page.component';
+import { JobsPageComponent } from './jobs-page/jobs-page.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfilePageComponent,
+    FeedPageComponent,
+    NetworkPageComponent,
+    JobsPageComponent,
+    ChatPageComponent
   ],
   imports: [
     BrowserModule,
