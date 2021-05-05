9fc9107e0d1744875fda370ab4f943f35a5ab883
Art S <art.worknsk@gmail.com>
2021-05-05 21:48:25 +0700

    add font, create structure scss, add header layout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  user = {
+    firstName: 'Sara',
+    lastName: 'Black',
+    email: '',
+    avatar: '../../assets/user.jpeg',
+    newViews: 32,
+    totalViews: 324,
   }
 
+  constructor() {}
+
+  ngOnInit() {
+  }
 }
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,15 @@
+// import {Injectable} from '@angular/core';
+// import {Observable, Subject} from 'rxjs';
+// import {User} from '../service/interfaces';
+
+// @Injectable({providedIn: 'root'})
+// export class UserService {
+//   user: any;
+//   // public user: User;
+//   constructor(user: User) {
+//   }
+
+//   getUserAvatar() {
+//     return this.user.avatar;
+//   }
+// }
