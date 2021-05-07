0d5b05c613ad15cf88f9d823acc38c952f500bca
Art S <art.worknsk@gmail.com>
2021-05-07 12:22:51 +0700

    add tabs logic & layout

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -5,9 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss']
 })
+
 export class ProfilePageComponent implements OnInit {
 
   constructor() { }
+
+  changeTab($event: any, tabName: string) {
+    let currentTab;
+    const tabs = document.getElementsByClassName('profile__tab') as HTMLCollectionOf<HTMLElement>;
+    const tabButtons = document.getElementsByClassName('profile__tab-button') as HTMLCollectionOf<HTMLElement>;
+
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].classList.remove('profile__tab--active');
+    }
+
+    for (let i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].className = tabButtons[i].className.replace(' profile__tab-button--active', '');
+    }
+
+    currentTab = document.getElementById(tabName);
+
+    if (currentTab) {
+      currentTab.classList.add('profile__tab--active');
+      $event.currentTarget.className += ' profile__tab-button--active';
+    }
+  }
 
   ngOnInit(): void {
   }
