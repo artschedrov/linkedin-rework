fc1c959636b58317db0eff7976e401c22284f730
Art S <art.worknsk@gmail.com>
2021-05-29 00:33:33 +0700

    add chat room layout

--- src/app/user/chat-page/shared/mock-chat-rooms.ts
+++ src/app/user/chat-page/shared/mock-chat-rooms.ts
@@ -17,7 +17,7 @@ export const CHAT_ROOMS: ChatRoom[] = [
         id: '23445',
         author: 'Sara Black',
         userPic: '../../../../assets/user.jpeg',
-        text: 'Hi',
+        text: 'Hi. How are you doing? Did you get that job yesterday?',
         time: '6:49 PM',
         currentUser: true
       },
@@ -27,12 +27,29 @@ export const CHAT_ROOMS: ChatRoom[] = [
         userPic: '../../../../assets/img/PhotoV1.png',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
         currentUser: false
+      },
+      {
+        id: '34445',
+        author: 'Darlene Black',
+        userPic: '../../../../assets/img/PhotoV1.png',
+        text: 'Lorem ipsum dolor sit amet.',
+        currentUser: false
       }
     ]
   },
   {
     id: '222',
     companion: 'Theresa Steward',
+    messages: [
+      {
+        id: '23445',
+        author: 'Sara Black',
+        userPic: '../../../../assets/user.jpeg',
+        text: 'Hi. How are you doing? Did you get that job yesterday?',
+        time: '6:49 PM',
+        currentUser: true
+      }
+    ]
   },
   {
     id: '333',
